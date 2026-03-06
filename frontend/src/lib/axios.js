4fcc303c1902b1b559e31ca604ddafcf6a204787
shreya1950 <chmcs2026.shreya.shinde@gmail.com>
2026-03-06 19:25:16 +0530

    Update axios.js

--- frontend/src/lib/axios.js
+++ frontend/src/lib/axios.js
@@ -1,5 +1,6 @@
 import axios from "axios";
 const api = axios.create({
-    baseURL: 'http://localhost:3000'
+   // baseURL: 'http://localhost:3000'
+    baseURL: 'https://donation-system-6u77.onrender.com'
 })
 export default api
